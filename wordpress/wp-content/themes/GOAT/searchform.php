3ef1733f2b63db29c4e7a584bdb3f68fad6744c7
Noham <72859612+Noh4m@users.noreply.github.com>
2022-03-12 08:11:16 +0100

    shearchBar css

--- wordpress/wp-content/themes/GOAT/searchform.php
+++ wordpress/wp-content/themes/GOAT/searchform.php
@@ -4,12 +4,15 @@
  */
 ?>
 
+<section class="searchBar">
+    <h1 class="">Où souhaitez-vous partir ?</h1>
+    <form action="<?php esc_url(home_url('/'))?>">
 
-<form action="<?php esc_url(home_url('/'))?>">
+        <input type="search" name="s" placeholder="Rechercher une ville..." aria-label="Search" value="<?= get_search_query()?>">
 
-	<input type="search" name="s" placeholder="Chercher" aria-label="Search" value="<?= get_search_query()?>">
+        <input type="text" hidden readonly name="page_id" value="18">
 
-	<input type="text" hidden readonly name="page_id" value="18">
+        <button type="submit" >Search</button>
+    </form>
+</section>
 
-	<button type="submit" >Search</button>
-</form>
