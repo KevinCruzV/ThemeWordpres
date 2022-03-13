4a19b694479ac08be17f0344e982dff823df46d4
Kevin <kevin.lacroix@hetic.net>
2022-03-13 22:09:44 +0100

    Update page about

--- wordpress/wp-content/themes/GOAT/template-page/_tpl-about-page.php
+++ wordpress/wp-content/themes/GOAT/template-page/_tpl-about-page.php
@@ -1,9 +1,16 @@
 <?php 
 /*
-* Template Name: About Page
+* Template Name:Template page about
 */
 get_header();
 ?>
-<h2>About page</h2>
+
+<?php if(have_posts()) :?>
+	<div>
+		<?php while (have_posts()) : the_post();?>
+        <h1 class="title-page"><?php the_title();?></h1>
+		<?php endwhile;?>
+	</div>
+<?php endif;?>
 
 <?php get_footer(); ?>
