dcff34605a998f022e92bae328bfa5a3a5cbf830
Kevin <kevin.lacroix@hetic.net>
2022-03-13 23:01:09 +0100

    Update page about

--- wordpress/wp-content/themes/GOAT/template-page/_tpl-about-page.php
+++ wordpress/wp-content/themes/GOAT/template-page/_tpl-about-page.php
@@ -6,11 +6,37 @@ get_header();
 ?>
 
 <?php if(have_posts()) :?>
-	<div>
+
+	<div class="presentation-projet">
 		<?php while (have_posts()) : the_post();?>
         <h1 class="title-page"><?php the_title();?></h1>
+		<div class="presentation-projet__p"><?php the_content(); ?></div>
 		<?php endwhile;?>
 	</div>
+
+	<div class="team-projet">
+		<h2>L'équipe</h2>
+		<div class="team-projet__container">
+			<div class="team-projet__container-cart">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/kevinc.jpg" alt="">
+				<h3><strong>Kévin Cruz</strong><br>Back-end Développeur</h3>
+			</div>
+			<div class="team-projet__container-cart">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/hugo.jpg" alt="">
+				<h3><strong>Hugo Cachon</strong><br>Back-end Développeur</h3>
+			</div>
+			<div class="team-projet__container-cart">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/noham.jpg" alt="">
+				<h3><strong>Noham Hirep</strong><br>Front-end Développeur</h3>
+			</div>
+			<div class="team-projet__container-cart">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/kevinl.jpg" alt="">
+				<h3><strong>Kévin Lacroix</strong><br>FullStack Développeur</h3>
+			</div>
+		</div>
+	</div>
+
+
 <?php endif;?>
 
 <?php get_footer(); ?>
